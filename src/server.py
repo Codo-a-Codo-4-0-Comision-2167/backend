9dceb810313fb62912931aab502ea70c631c445a
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2021-11-29 23:15:48 -0300

    server using json as string

--- src/server.py
+++ src/server.py
@@ -1,13 +1,20 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler
+from http.server import HTTPServer
+import os
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/html')
+        self.send_header('Content-type','application/json')
         self.end_headers()
-
-        message = "Hello, World!"
-        self.wfile.write(bytes(message, "utf8"))
+        script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+        rel_path_folder = "data"
+        abs_file_path = os.path.join(os.path.join(script_dir, rel_path_folder), 'mydata.json')
+        
+        with open(abs_file_path, 'r') as f:
+            self.wfile.write(f.read().encode("utf8"))
+        #message = "{ 'Hello': 'World!' }"
+        #self.wfile.write(bytes(message, "utf8"))
     
     def do_POST(self):
         self.send_response(200)
